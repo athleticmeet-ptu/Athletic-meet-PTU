352fef46b29b7a14adf8a67d661007632f867cce
athleticmeet-ptu <athleticptu@gmail.com>
2025-03-14 08:01:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_API_URL': JSON.stringify('https://athletic-backend.vercel.app/')
+    'process.env.VITE_API_URL': JSON.stringify('https://athletic-backend.vercel.app')
   },
   server: {
     historyApiFallback: true, // Ensure fallback to index.html
     proxy: {
       '/api': {
-        target: 'https://athletic-backend.vercel.app/',
+        target: 'https://athletic-backend.vercel.app',
         changeOrigin: true,
         secure: false
       }
