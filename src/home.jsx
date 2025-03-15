5aa166e78319d2b3851e48eccd089e3f37f546e3
athleticmeet-ptu <athleticptu@gmail.com>
2025-03-15 21:48:23 +0530

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -22,7 +22,7 @@ function Home() {
   return (
     <div className="home-container">
       <header className="header">
-        <nav className="navbar">
+        <nav className="navbar-nav">
           <h1 className="logo">26th IKGPTU Athletic Meet</h1>
           <ul className="nav-list">
             <button className="contact" onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })}>
